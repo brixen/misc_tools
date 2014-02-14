4b30a7d6eeb3e2fbbbbd404027ef6ff794c0f6e5
Brian Shirai <brixen@gmail.com>
2014-02-13 18:03:50 -0600

    Export Sankey diagram data.

--- top_connected_classes.rb
+++ top_connected_classes.rb
@@ -3,6 +3,7 @@ require 'json'
 input = ARGV.shift
 output = ARGV.shift
 omit_self = ARGV.shift
+sankey = ARGV.shift
 
 puts "Importing #{input}..."
 data = JSON.parse File.read(input)
@@ -37,10 +38,35 @@ end
 table = table.first 50
 
 puts "Writing #{output}..."
-File.open output, "w" do |f|
-  id = 0
-  f.puts %["id","class1","class2","flow1","flow2"]
+if sankey
+  id = -1
+  classes = Hash.new { |h,k| h[k] = (id += 1) }
+
   table.each do |x|
-    f.puts %[#{id+=1},"#{x[0]}","#{x[1]}",#{x[2]},#{x[3]}]
+    classes[x[0]]
+    classes[x[1]]
+  end
+
+  dict = {}
+
+  nodes = []
+  classes.inject(nodes) { |ac, x| ac << { "name" => x.first } }
+  dict["nodes"] = nodes
+
+  links = []
+  table.inject(links) do |ac, x|
+    ac << { "source" => classes[x[0]], "target" => classes[x[1]], "value" => x[2] }
+  end
+  dict["links"] = links
+  File.open output, "w" do |f|
+    f.write JSON.generate(dict)
+  end
+else
+  File.open output, "w" do |f|
+    id = 0
+    f.puts %["id","class1","class2","flow1","flow2"]
+    table.each do |x|
+      f.puts %[#{id+=1},"#{x[0]}","#{x[1]}",#{x[2]},#{x[3]}]
+    end
   end
 end
